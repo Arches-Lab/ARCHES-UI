e8258675d7010b014e3a39068bd78193e8ed8a7e
manojuppal <manojuppal@hotmail.com>
2025-08-06 09:20:46 -0700

    StoreOperations

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import { FaTachometerAlt, FaUser, FaCog, FaUsers, FaEnvelope, FaLightbulb, FaInbox, FaUserPlus, FaTasks, FaExclamationTriangle, FaBoxes, FaCalendarAlt } from 'react-icons/fa';
+import { FaTachometerAlt, FaUser, FaCog, FaUsers, FaEnvelope, FaLightbulb, FaInbox, FaUserPlus, FaTasks, FaExclamationTriangle, FaBoxes, FaCalendarAlt, FaStore } from 'react-icons/fa';
 
 const links = [
   { to: '/', label: 'Dashboard', icon: <FaTachometerAlt /> },
@@ -11,6 +11,7 @@ const links = [
   { to: '/tasks', label: 'Tasks', icon: <FaTasks /> },
   { to: '/incidents', label: 'Incidents', icon: <FaExclamationTriangle /> },
   { to: '/supplies', label: 'Supplies', icon: <FaBoxes /> },
+  { to: '/store-operations', label: 'Operations', icon: <FaStore /> },
   { to: '/profile', label: 'Profile', icon: <FaUser /> },
   // { to: '/settings', label: 'Settings', icon: <FaCog /> },
   { to: '/newuser', label: 'User Accounts', icon: <FaUserPlus /> },
